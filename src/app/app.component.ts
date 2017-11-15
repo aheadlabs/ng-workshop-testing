c0eed44ac5dc805a6958cd16287022eaaf57eb50
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2017-11-15 15:21:37 +0100

    Add filter users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  originalUsers: any[];
   users: any[];
 
   constructor(private httpClient: HttpClient) {
@@ -18,9 +19,24 @@ export class AppComponent {
     this.httpClient.get<any[]>('http://localhost:3001/api/public/users').subscribe(
       users => {
         console.log(users);
+        this.originalUsers = users;
         this.users = users;
       }
     );
+  }
+
+  filter(username: string) {
+    let matched = false;
+    this.users.forEach(user => {
+      if (user.username === username) {
+        matched = true;
+        this.users = [];
+        this.users.push(user);
+      }
+    });
+    if (this.users.length === 0 || !matched) {
+      this.getUsers();
+    }
   }
 
 }
